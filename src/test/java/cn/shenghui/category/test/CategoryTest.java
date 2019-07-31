d4a90f8230320a75fa8186857fe13d2d9ad11f0f
Alkali_Shift <swordsgod1111@icloud.com>
2019-07-31 10:30:26 +0800

    test/finished - demo

---
+++ src/test/java/cn/shenghui/category/test/CategoryTest.java
@@ -0,0 +1,95 @@
+package cn.shenghui.category.test;
+
+import cn.shenghui.CategoryApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+/**
+ * @author shenghui
+ * @version 1.0
+ * @since 2019/7/31 09:17
+ * category test
+ */
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = CategoryApplication.class)
+public class CategoryTest {
+    private MockMvc mvc;
+
+    @Autowired
+    public void setMockMvc(MockMvc mockMvc) {
+        this.mvc = mockMvc;
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testCreateCategory() throws Exception{
+        mvc.perform(MockMvcRequestBuilders
+                .post("/category/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "  \"categoryName\": \"testName\",\n" +
+                        "  \"parentAncestors\": \"0,100,102\",\n" +
+                        "  \"parentId\": 105\n" +
+                        "}")
+        )
+                .andExpect(jsonPath("$.statusCode").value(1))
+                .andReturn();
+    }
+
+    @Test
+    public void testListCategory() throws Exception{
+        mvc.perform(MockMvcRequestBuilders
+                .get("/category/list")
+                .param("parentId", "100")
+                .param("categoryId", "101")
+        )
+                .andExpect(jsonPath("$.statusCode").value(1))
+                .andReturn();
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testDeleteCategory() throws Exception{
+        mvc.perform(MockMvcRequestBuilders
+                .post("/category/delete")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "  \"ancestors\": \"0,100,102\",\n" +
+                        "  \"categoryId\": 105\n" +
+                        "}")
+        )
+                .andExpect(jsonPath("$.statusCode").value(1))
+                .andReturn();
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testUpdateCategory() throws Exception{
+        mvc.perform(MockMvcRequestBuilders
+                .post("/category/update")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "  \"categoryId\": 105,\n" +
+                        "  \"parentId\": 100,\n" +
+                        "  \"targetCategoryId\": 102\n" +
+                        "}")
+        )
+                .andExpect(jsonPath("$.statusCode").value(1))
+                .andReturn();
+    }
+}
