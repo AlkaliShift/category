f70f3274d5033a0966a1489a7ef418adb8b318e7
swordsgod1111 <swordsgod1111@icloud.com>
2019-08-06 21:54:19 +0800

    front-end: list category/finished

--- src/main/resources/static/js/category.js
+++ src/main/resources/static/js/category.js
@@ -3,30 +3,42 @@ $(document).ready(function () {
         var tree = layui.tree
             , layer = layui.layer
             , util = layui.util
-            , data = [{title: 'Category', id: 100}];
+            , data = [{title: 'Category', id: 100, spread: true}];
 
-        // $("#test").val("");
         tree.render({
             elem: '#test'
             , data: data
             , id: "tree"
             , edit: ['add', 'update', 'del']
             , click: function (obj) {
-                //var children = getList(obj.data.id).categories;
-                obj.data.children = {title: "1", id: 101};
-                var temp = [];
-                temp.title = obj.data.title;
-                temp.id = obj.data.id;
-                temp.children = obj.data.children;
-
-                reloadTree(temp);
+                var tempData = search(obj, data);
+                reloadTree(tempData);
             }
         });
 
-        function reloadTree(temp){
-            tree.reload("tree",{
+        function reloadTree(temp) {
+            tree.reload("tree", {
                 data: temp
             });
+        }
+
+        function search(obj, temp){
+            for (i in temp) {
+                var id = temp[i].id;
+                if (obj.data.id === id) {
+                    var children = getList(obj.data.id).categories;
+                    if (children == null){
+                        return;
+                    }
+                    temp[i].children = [];
+                    for (j in children) {
+                        temp[i].children.push({title: children[j].categoryName, id: children[j].categoryId, spread: true});
+                    }
+                }else{
+                    search(obj, temp[i].children);
+                }
+            }
+            return temp;
         }
     });
 });
@@ -41,8 +53,12 @@ function getList(parentId) {
         dataType: "json",
         async: false,
         success: function (result) {
-            flag = 1;
-            data = result;
+            if (result.statusCode === 1) {
+                flag = 1;
+                data = result;
+            } else {
+                flag = 2;
+            }
         },
         error: function () {
         }
@@ -50,6 +66,8 @@ function getList(parentId) {
 
     if (flag == 1) {
         return data;
+    } else if (flag == 2) {
+        return null;
     } else {
         return "Connection failed.";
     }
@@ -57,42 +75,42 @@ function getList(parentId) {
 
 var data1 = [{
     title: '江西'
-    ,id: 1
-    ,children: [{
+    , id: 1
+    , children: [{
         title: '南昌'
-        ,id: 1000
-        ,children: [{
+        , id: 1000
+        , children: [{
             title: '青山湖区'
-            ,id: 10001
-        },{
+            , id: 10001
+        }, {
             title: '高新区'
-            ,id: 10002
+            , id: 10002
         }]
-    },{
+    }, {
         title: '九江'
-        ,id: 1001
-    },{
+        , id: 1001
+    }, {
         title: '赣州'
-        ,id: 1002
+        , id: 1002
     }]
-},{
+}, {
     title: '广西'
-    ,id: 2
-    ,children: [{
+    , id: 2
+    , children: [{
         title: '南宁'
-        ,id: 2000
-    },{
+        , id: 2000
+    }, {
         title: '桂林'
-        ,id: 2001
+        , id: 2001
     }]
-},{
+}, {
     title: '陕西'
-    ,id: 3
-    ,children: [{
+    , id: 3
+    , children: [{
         title: '西安'
-        ,id: 3000
-    },{
+        , id: 3000
+    }, {
         title: '延安'
-        ,id: 3001
+        , id: 3001
     }]
 }]
